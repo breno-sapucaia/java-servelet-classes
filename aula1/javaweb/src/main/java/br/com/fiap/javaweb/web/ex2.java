d79f907aa4fa54ba480bee3db5f4f6420f022f48
Logon Aluno <logonaluno@local.com>
2019-08-08 11:55:24 -0300

    aula 1 finalizada, terminal lista de exercicios

--- aula1/javaweb/src/main/java/br/com/fiap/javaweb/web/ex2.java
+++ aula1/javaweb/src/main/java/br/com/fiap/javaweb/web/ex2.java
@@ -1,8 +1,10 @@
 package br.com.fiap.javaweb.web;
-
+import java.text.SimpleDateFormat;
 import java.io.IOException;
-
+import java.io.PrintWriter;
+import java.util.Date;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -14,9 +16,15 @@ public class ex2 extends HttpServlet{
 	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		
+		Date data = new Date();
+		SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy");
+		
+		PrintWriter out = resp.getWriter();
+		
+		out.println("A data atual é "+format.format(data) );
 	}
+	
 	
 	
 }
